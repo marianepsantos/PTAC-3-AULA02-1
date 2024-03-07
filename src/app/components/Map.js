97cecb35a27ca011943048f3d7a13a4cd05a96ef
marianepsantos <marianepsantos15@gmail.com>
2024-03-07 11:16:01 -0300

    Commit
    importei o css do footer e do menu em not found, e criei um button para voltar a pagina home. Criei tbm uma class no map para assim conseguir fazer o csss

--- src/app/components/Map.js
+++ src/app/components/Map.js
@@ -4,11 +4,11 @@ import styles from "./map.module.css"
 export default function Map() {
     return(
         <iframe className={styles.map}
-            id="gmap_canvas"
-            src={"https://maps.google.com/maps?"}
-            frameborder="0" scrolling="no"
-            mirginheight="0"
-            marginwidth="0">
-        </iframe>
+          id="gmap_canvas"
+          src={"https://maps.google.com/maps?       q=Nova%20Andradina%20+IFMS+&t=&z=13&ie=UTF8&iwloc=&output=embed"}
+          frameborder="0" scrolling="no"
+          marginheight="0"
+          marginwidth="0">
+          </iframe>
     );
 }
--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,12 +1,14 @@
 import styles from './components/not-found.module.css'
 import Link from 'next/link'
+import "./components/menu.module.css"
+import "./components/footer.module.css"
 
 
 export default function NotFound(){
     return (
        <div className="not-found">
         <h2 className={styles.Erro}>Página não encontrada</h2>
-        <a href="/">Retornar para Home</a>
+        <button>  <a href= "/"> Voltar para a Home </a> </button> 
         </div>     
     )
 };
